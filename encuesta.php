4b02436efcc56c0b2db485fff6837f4d850d66c6
oswaldo25 <104531658+oswaldo25@users.noreply.github.com>
2022-05-02 09:31:20 -0500

    Add files via upload

---
+++ encuesta.php
@@ -0,0 +1,291 @@
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+	<!-- meta tags requeridos -->
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+	<meta name="author" content="Universidad de Colima"/>
+	<meta http-equiv="x-ua-compatible" content="ie=edge"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:description" content="Universidad de Colima"/>
+	<meta property="og:image" content="http://wayf.ucol.mx/ucol_mini.png"/>
+	<meta property="og:locale:alternate" content="es_ES" />
+	<meta property="og:site_name" content="Universidad de Colima" />
+		
+	<!-- Titulo principal -->
+	<title>HDUNI</title>
+	<!-- link requeridos -->
+	<link href="http://wayf.ucol.mx/ucol_mini.png" rel="image_src" />
+	<link href="//www.ucol.mx/cms/img/favicon.ico" type="image/x-icon" rel="icon" />
+	<!-- jQuery -->
+	<script src="//www.ucol.mx/cms/beta/js/jquery.min.js"></script> 
+	<!-- Bootstrap y header & footer agregados -->
+	<link href="//www.ucol.mx/cms/headerfooterapp2.css?v=1" rel="stylesheet">
+	<link href="//www.ucol.mx/cms/beta/css/carrusel.css" rel="stylesheet">
+    
+  
+    <style>
+		.breadcrumb li {
+			font-size: 14px !important;
+		}
+		.page-breadcrumb a.btn-danger {
+			padding: 0 15px;
+			color: #fff !important;
+		}
+		
+		body {font-family: Arial;}
+		input[type=text], input[type=email], input[type=tel], input[type=file],select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+	</style>
+  </head>
+  <body>
+  	<div id="estructura">
+	<!-- Fixed navbar -->
+	<nav class="navbar navbar-light bg-faded theme-primary pos-f-t">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-12 col-lg-12 col-md-12 col-xs-12">
+					<button class="navbar-toggler hidden-md-up pull-right collapsed" type="button" data-toggle="collapse" data-target="#navbar-header" aria-controls="navbar-header" aria-expanded="false">&#x2630;</button>			        <div class="collapse navbar-toggleable-sm" id="navbar-header">
+				        
+			            <a class="navbar-brand" id="logo" href="//www.ucol.mx/" target="_blank">Universidad de Colima</a>
+			            <span class="home-href"><a href="./"></a></span>
+			            						   <!--<ul id="navlist" class="nav navbar-primary navbar-nav pull-md-right">
+						   <li class="nav-item">
+						      <a class="nav-link" data-hover="dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">
+						         Opcion 1
+						      </a>
+						           <ul id="subnavlist" class="dropdown-menu">
+						              <li><a href="#">Sub opcion 1</a></li>
+						              <li><a href="#">Sub opcion 2</a></li>
+						           </ul>
+						   </li>
+						   <li class="nav-item">
+						      <a class="nav-link" href="#">
+						         Opcion 2
+						      </a>
+						   </li>
+						   <li class="nav-item">
+						      <a class="nav-link" href="#">
+						         Opcion 3
+						      </a>
+						   </li>
+						</ul>-->
+			        </div>
+				</div>
+			</div>
+		</div>
+    </nav> <!-- /navbar -->
+     <section class="page-breadcrumb"> 
+     <div class="container "> 
+     	<div id="path">
+	        	<ol class="breadcrumb">
+	            	<!--<li>Usted está en:</li> -->
+	           <!--  <li><a href="cdeprincipal.php">Inicio</a></li> -->
+				
+				   <li><a href="#">Capturador</a></li>
+			</ol>
+        </div>
+        <div id="sesion">
+	        	<ol class="breadcrumb">
+	            	
+	            <li class="user-name"><?php echo $nombrea; ?></li>
+	            <li><a href="logout.php">Salir</a></li>
+			</ol>
+        </div>
+     </div> <!--cierra path-->
+    </section>
+	<section class="page-header">
+        <div class="container">
+	        <h1 class="title-ucol">HISTORIA DEL DEPORTE UNIVERSITARIO (HDUNI)</h1>
+	    </div><!--/ Cierra .container /-->
+    </section>
+    <nav class="nav-sistema">
+    		<button class="navbar-toggler hidden-md-up pull-right collapsed" type="button" data-toggle="collapse" data-target="#navbar-header2" aria-controls="navbar-header2" aria-expanded="false">☰</button>
+		<div class="collapse navbar-toggleable-sm" id="navbar-header2">
+	        <ul id="navlist" class="nav navbar-primary navbar-nav pull-md-right">
+			    <li class="nav-item">
+	                <a class="a3 nav-link" href="index.php">
+	                    <div class="oIcono opcion3"></div>
+	                    Inicio
+	                </a>
+	                <!--[if gte IE 7]><!--><!--<![endif]-->
+	            </li>
+	            <li class="nav-item">
+	                <a class="a1 nav-link" data-hover="dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" href="#">
+	                    <div class="oIcono opcion1"></div>
+	                    Noticias
+	                </a>
+	                <!--[if gte IE 7]><!--><!--<![endif]--><!--[if lte IE 6]>
+	                <table>
+	                    <tr>
+	                        <td>
+	                            <![endif]-->
+	                            <ul id="subnavlist" class="dropdown-menu">
+	                                <li><a href="registrarn.php">Registrar</a></li>
+				                    <li><a href="consultarn.php">Consultar</a></li>
+				                    <!--<li><a href="cdeconsulta.php">Consulta</a></li>
+				                    <li><a href="#Paris">Consultas</a></li> -->
+	                            </ul>
+	                            <!--[if lte IE 6]>
+	                        </td>
+	                    </tr>
+	                </table>
+	                </a><![endif]-->
+	            </li>
+	           
+	        <!--    <li class="nav-item">
+	                <a class="a3 nav-link" href="#">
+	                    <div class="oIcono opcion3"></div>
+	                    Nutricion
+	                </a>
+	                
+	            </li> -->
+	        </ul>
+	    </div>
+	</nav>
+    
+    
+ <div class="container c-principal">
+  <div class="row p-contenido">
+		<div class="col-xl-2 col-lg-4 col-md-4 col-xs-12 sidebar">
+		   <h3>Noticias</h3>
+      	   <p>Registrar Noticia</p>
+			
+  	 
+			</div>
+			 <div class="col-xl-8 col-lg-8 col-md-8 col-xs-12 main"> <!--Con  contenido izquierdo o derecho uno de los dos
+			<div class="col-xl-12 col-lg-12 col-md-12 col-xs-12 main"> 
+			<div class="col-xl-6 col-lg-4 col-md-4 col-xs-12 main"> 
+	<div class="col-xl-12 col-lg-12 col-md-12 col-xs-12 main" align="center"> -->
+		
+	     <form class="w3-container w3-card-4 w3-padding-16 w3-white" action="encuesta.php" method="post" enctype="multipart/form-data">
+	    <div class="w3-section"> 
+	    	<label><b>Sabes o has escuchado del termino Cloud Computing (Computo en la nube)?</b></label><br>
+            <input type="radio" id="html" name="respuesta1" value="Si">
+             <label for="html">Si</label><br>
+             <input type="radio" id="css" name="respuesta1" value="No">
+            <label for="css">No</label><br>
+		</div>
+		<div class="w3-section"> 
+	    	<label>Cree que se beneficie FIME por la enseñanza de Cloud Computing?</label><br>
+            <input type="radio" id="html" name="respuesta2" value="Si">
+             <label for="html">Si</label><br>
+             <input type="radio" id="css" name="respuesta2" value="No">
+            <label for="css">No</label><br>
+		</div>
+		
+        <button type="submit" name="submit1" >Agregar</button>
+      </form>
+      <?php
+		///////////////////////////////  MK USUAURIOS ALTAS  ////////////////////////////////////
+		if(isset($_POST['submit1'])){
+			$respuesta1 =  $_POST['respuesta1'];
+			$respuesta2 =  $_POST['respuesta2'];
+		
+			// 1.- IDENTIFICACION nombre de la base, del usuario, clave y servidor
+			require_once('conexiona.php');
+
+			// 2.- CONEXION A LA BASE DE DATOS
+			$link = new mysqli($db_host, $db_login, $db_pswd, $db_name);
+			
+		         $tabla='computo';
+			    $query2="insert into $tabla (respuesta1, respuesta2) VALUES ('$respuesta1','$respuesta2')";
+		 	    $result = mysqli_query($link, $query2);
+		    	//echo '<script>alert("Noticia registrada al sistema.")</script>';
+	 	   
+		        if($result){
+                     echo '<script>alert("encuesta registrada y subida correctamente.")</script>';
+            	}else{
+                    echo '<script>alert("Ha fallado la subida de la encuesta. Intente nuevamente.")</script>';
+       		     } 
+            	
+		 mysqli_close($link);
+		}
+		?>	     
+		
+	</div>
+
+			<div class="col-xl-2 col-lg-4 col-md-4 col-xs-12 sidebar">
+				
+			</div> 
+  </div>
+		<div class="row p-inferior">
+			<div class="col-md-12 p-content"></div>
+		</div>
+ </div>
+
+</div>
+ 
+    <footer class="bd-footer text-muted" role="contentinfo">
+	    <div class="container">
+		    <div class="row">
+			    <div class="col-xl-12 col-lg-12 col-md-12 col-xs-12 center-links">
+				    <img src="//www.ucol.mx/cms/beta/img/icon/ubicacion.svg" width="20"> Direcci&oacute;n: Av. Universidad No. 333, Las V&iacute;boras; CP 28040 Colima, Colima, M&eacute;xico
+			    </div>
+			    <div class="col-xl-12 col-lg-12 col-md-12 col-xs-12 center-links">
+				    &copy; Derechos Reservados 2018-2021 Universidad de Colima
+			    </div>
+		    </div>
+	    </div>
+	</footer>
+    
+    <!-- JS Bootstrap -->
+    <script src="//www.ucol.mx/cms/beta/dist/js/tether.min.js" type="text/javascript"></script>
+    <script src="//www.ucol.mx/cms/beta/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="//www.ucol.mx/cms/beta/dist/js/bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
+    <!-- JS adds -->
+    <script src="//www.ucol.mx/cms/beta/js/jquery.slides.js" type="text/javascript"></script>
+    <script src="//www.ucol.mx/cms/beta/js/carruselV3.2.js" type="text/javascript"></script>
+    <script src="//www.ucol.mx/cms/beta/js/jquery.flexisel0815.js" type="text/javascript"></script>
+    <script src="//www.ucol.mx/cms/beta/js/purl.js"></script>
+    <script src="//www.ucol.mx/cms/beta/js/custom.min.js" type="text/javascript"></script>
+    <script src="//www.ucol.mx/cms/js/custom.js" type="text/javascript"></script>
+    <!-- HTML5 shim y Respond.js para soporte IE8 de elementos HTML5 y media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <!--script>
+        // Carousel Auto-Cycle
+		$(document).ready(function() {
+			//$('#navcontainer > #navlist').attr('data-spy','affix').attr('data-offset-top',100).attr('data-offset-bottom', 531);
+			$('#sliderNaN>.rslides').responsiveSlides({auto:true,pager:true,nav:true,timeout:6000,speed:800});
+			$(function () {
+			  $('[data-toggle="tooltip"]').tooltip()
+			})
+		    $( '.form-search-ucol' ).click( function() {
+				$( this ).addClass( 'active' );
+			}).focusout( function() {
+				$( this ).removeClass( 'active' );
+			});
+			$('#navcontainer > #navlist').affix({
+			  offset: {
+			    top: 100,
+			    bottom: function () {
+			      return (this.bottom = $('footer').outerHeight(true)+100)
+			    }
+			  }
+			});
+			var modWidth = $( '#navcontainer' ).width() - 30;
+		    $( '#navlist' ).width( modWidth );
+			jQuery( window ).resize( function () {
+		        if ( jQuery( window ).width() ) {
+			        var modWidth = $( '#navcontainer' ).width() - 30;
+		           $( '#navlist' ).width( modWidth );
+		        }
+			});
+
+			
+		});
+    </script-->
+			
+  </body>
+</html>
